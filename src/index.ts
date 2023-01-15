68b72a46ddb98ac795cfb4ac6aaecb639f2ef894
Artiom Karimov <lunatic0259@gmail.com>
2023-01-15 17:16:27 +0300

    added ability to connect

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { config } from 'dotenv';
 config();
+import * as amqp from 'amqplib';
 
+let connection: amqp.Connection;
+let channel: amqp.Channel;
 
+const connect = async () => {
+  try {
+    connection = await amqp.connect(process.env.RABBITMQ_CONNECTION || 'amqp://localhost:6572');
+    channel = await connection.createChannel();
+    await channel.assertQueue('test-queue');
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-console.log('hell-o');
+connect();
